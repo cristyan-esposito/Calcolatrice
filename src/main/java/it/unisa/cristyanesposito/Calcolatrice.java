3156d9e61aeb3b5fbfffb27301592eff722c2a10
Cristyan Esposito <c.esposito175@studenti.unisa.it>
2023-09-27 11:47:50 +0200

    Upload project

---
+++ src/main/java/it/unisa/cristyanesposito/Calcolatrice.java
@@ -0,0 +1,20 @@
+package it.unisa.cristyanesposito;
+
+public class Calcolatrice {
+
+    private int a, b;
+
+    public Calcolatrice () {
+        this.a = 0;
+        this.b = 0;
+    }
+
+    public int somma (int a, int b) {
+        return a + b;
+    }
+
+    public int moltiplicazione (int a, int b) {
+        return a * b;
+    }
+
+}
